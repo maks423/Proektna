7cb6b02b466cf0ff59bfc824940ee65d24c0b567
maks123 <maks4236843@gmail.com>
2023-03-24 20:57:43 +0200

    changes

--- src/pages/doctor-page/doctor-page.jsx
+++ src/pages/doctor-page/doctor-page.jsx
@@ -4,30 +4,28 @@ import "./doctor-page.css";
 const DoctorPage = () => {
   return (
     <div className="backgroundHelloDoctor">
+      <span className="profileText">Name</span>
+      <span className="profileText">Last name</span>
+      <div className="porfileSpace">
+        <img className="iconProfile" src="./images/Icon_profile.png"></img>
+      </div>
+
       <div className="sectionDoctor">
-        <div className="headerTextDoctor">Hello,doctor</div>
-
-        <div className="avatarDoctor">
-            <img className="cyrcleDoctor"></img>
-        </div>
-
-        <div className="contactInformation">
-          <label className="textInputDoctor" for="fname">Name</label>
-          <input className="doctorPageInput"type="text"id="fname" name="firstname"
-          />
-          <label className="textInputDoctor" for="lname">Last Name</label>
-          <input className="doctorPageInput"type="text"id="lname" name="lastname"/>
-        </div>
-
-        <div className="startMeetingDoctor">
-          <input className="startMeetingButtonDoctor"type="button"value="Start meeting"/>
-        </div> 
         
+        <div className="startMeetingDoctor">
+          <input
+            className="startMeetingButtonDoctor"
+            type="button"
+            value="Start meeting"
+          />
+        </div>
+
         <div className="clientBase">Client Base</div>
 
         <div className="clientBazeArea">
-          <div className="textAreaClientBaze">
-          </div>
+          <span className="textClientBase">Name</span>
+          <span className="textClientBase">Last name</span>
+          <div className="textAreaClientBaze"></div>
         </div>
       </div>
     </div>
